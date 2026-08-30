7572caa94d1497c7053b9c1566a8ef4eb3560041
es-sai-fi <athousandheavenlycats@protonmail.com>
2026-08-29 23:52:59 -0500

    installations/channels: update

--- installations/channels/configuration.nix
+++ installations/channels/configuration.nix
@@ -1,56 +1,97 @@
-{ config, pkgs, lib, ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
+  config,
+  modules,
+  pkgs,
+  ...
+}:
+{
+  imports = with modules; [
+    # WARN: Check out the comments in this file too.
+    ./hardware-configuration.nix
 
-  nixpkgs.pkgs = import <nixpkgs> { };
+    nix-daemon
+    openssh
+    sysklogd
+    limine
+    sudo
+    polkit
+    # WARN: Either import this or import and enable ly's module, if you don't
+    # you will be left with an unbootable generation.
+    getty
+    bash
+    dhcpcd
+    iwd
+  ];
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  finit.runlevel = 3;
 
   boot.loader.efi.canTouchEfiVariables = true;
 
-  services.nix-daemon.enable = true;
+  programs = {
+    limine = {
+      enable = true;
+      settings.editor_enabled = true; # NOTE: Disable on systems that need security.
+    };
 
-  programs.limine = {
-    enable = true;
-    settings.editor_enabled = true;
+    sudo.enable = true;
+
+    bash.enable = true;
   };
-
-  programs.sudo.enable = true;
-  # programs.doas.enable = true;
-
-  programs.bash.enable = true;
 
   security.pam.environment = {
     NIX_PATH.default = "/root/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
   };
 
-  services.dbus.enable = true;
+  services = {
+    nix-daemon = {
+      enable = true;
+      settings = {
+        trusted-users = [
+          "root"
+          "@wheel"
+        ];
+      };
+    };
 
-  # Network options
-  services.iwd.enable = true; # cli network manager
-  # services.networkmanager.enable = true; # requires udev instead of mdev
-  # services.dhcpcd.enable = true; #best for ethernet
+    sysklogd.enable = true;
 
-  services.sysklogd.enable = true;
+    dbus.enable = true;
 
-  services.mdevd.enable = true;
+    mdevd.enable = true;
 
-  time.timeZone = "America/New_York";
+    # WARN: Are you sure you don't want to enable this? You'll have to configure
+    # stuff like DNS manually to be able to access the internet.
+    dhcpcd.enable = true;
+    iwd.enable = true;
+  };
 
-  networking.hostName = "finix";
+  # TODO: Define hostname.
+  networking.hostName = "<HOSTNAME>";
 
-  users.users.vitrial = {
+  # TODO: Set timezone.
+  time.timeZone = "<TIMEZONE>";
+
+  # TODO: Define your user. Create a password with:
+  # $ mkpasswd --method=yescrypt
+  users.users."<USERNAME>" = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    password = "$6$caDmJ4T2oF09Nrf6$XsQ9KDtgduEul9gzFmLjSZ.aypOKiQWuCicZIGcxPdpVNkGmF1AzNFCyR1u4yeEyZ9ryR8tgx8/u9mTyoGI1v/";
+    # WARN: Don't forget to set a hashed password here or you'll face the consequences
+    # of your actions.
+    password = "<HASHED_PASSWORD>";
   };
 
   environment.systemPackages = with pkgs; [
-    git
     vim
-    # nano
-    # micro
-    # emacs
-    nixos-rebuild-ng
+    wget
+    git
     iputils
     iproute2
+    # NOTE: You can get rid of this if you use nh but be aware you need either of
+    # them to rebuild your system...
+    nixos-rebuild-ng
   ];
 }
